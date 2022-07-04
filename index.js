a36da8fb10d164675332757c5008d5bb22a95f2d
Trugamr <trugamr@live.in>
2022-07-04 20:46:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ message: "hello 👋" }));
+});
+
+const port = process.env.PORT ?? 3000;
+
+server.listen(port, () => {
+  console.log(`Listening on ${port}...`);
+});
+
+server.on("error", (error) => {
+  console.error(error);
+  server.close();
+});
