badd2e47944464d10ac2829abd064ba80aeea2c8
Trugamr <trugamr@live.in>
2022-07-05 13:31:04 +0530

    update message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ message: "🐳 docker is awesome" }));
+  res.end(JSON.stringify({ message: "drone is nice" }));
 });
 
 const port = process.env.PORT ?? 3000;
